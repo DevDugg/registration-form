60c62e92c5ed7fe779e7f572795a105fbfd94e25
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-02-13 16:41:02 +0900

    Refactor form component and update imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
 import BlurredOverlay from "@/components/blurredOverlay";
-import { FormWrapper } from "@/components/form";
+import FormWrapper from "@/components/form";
+import GoogleButton from "@/components/googleButton";
 import Header from "@/components/header";
+import Separator from "@/components/separator";
+import Welcome from "@/components/welcome";
 
 export default function Home() {
   return (
     <main className="main">
       <BlurredOverlay height={"100vh"} width={"50vw"} className="content flex flex-col gap-20 px-10 py-14">
         <Header />
-        <FormWrapper />
+        <div className="flex justify-center">
+          <div className="form-wrapper flex flex-col items-center gap-10 max-w-[484px]">
+            <Welcome />
+            <GoogleButton />
+            <Separator text="or" />
+          </div>
+        </div>
       </BlurredOverlay>
     </main>
   );
--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,19 +1,3 @@
-import GoogleButton from "./googleButton";
-import Separator from "./separator";
-import Welcome from "./welcome";
-
 const Form = () => {};
 
-const FormWrapper = () => {
-  return (
-    <div className="flex justify-center">
-      <div className="form-wrapper flex flex-col items-center gap-10 max-w-[484px]">
-        <Welcome />
-        <GoogleButton />
-        <Separator text="or" />
-      </div>
-    </div>
-  );
-};
-
-export { GoogleButton, FormWrapper };
+export default Form;
